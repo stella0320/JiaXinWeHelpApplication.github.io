22f006abff58b1ee360a90525ac3f856674b27ba
stella0320 <jud40322@gmail.com>
2023-07-18 22:29:50 +0800

    week3 init v1

--- docs/task/week3/week3.py
+++ docs/task/week3/week3.py
@@ -1,6 +1,6 @@
 import urllib.request as request
-import json;
-import re
+import json
+# import re
 url = 'https://padax.github.io/taipei-day-trip-resources/taipei-attractions-assignment.json';
 with request.urlopen(url) as response:
     # 需要注意編碼
@@ -8,14 +8,14 @@ with request.urlopen(url) as response:
 
 location_info = data['result']['results'];
 
-distict_zone_list = ['中正區', '萬華區', '中⼭區', '⼤同區', '⼤安區', '松⼭區', '信義區', '⼠林區', '文⼭區', '北投區', '內湖區', '南港區'];
+# distict_zone_list = ['中正區', '萬華區', '中⼭區', '⼤同區', '⼤安區', '松⼭區', '信義區', '⼠林區', '文⼭區', '北投區', '內湖區', '南港區'];
 print(len(location_info));
 with open('attraction.csv', mode='w', encoding='utf-8') as file:
     for location in location_info:
         file.write(location['stitle']);
         file.write(',');
         distict = location['address'][5:8];
-        file.write(location['address'][5:8]);
+        file.write(distict);
         location['distict'] = distict;
         all_file = location['file'];
         position = all_file.index("https://www.travel.taipei/", 5);
@@ -23,15 +23,10 @@ with open('attraction.csv', mode='w', encoding='utf-8') as file:
         location['main_website'] = main_website;
         file.write(',');
         file.write(main_website);
-        # pattern = "^https?:\\/\\/(?:www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b(?:[-a-zA-Z0-9()@:%_\\+.~#?&\\/=]*)$";
-        # all_file_list = re.findall(pattern, all_file, re.IGNORECASE);
-        # if len(all_file_list) > 0:
-        #     file.write(',');
-        #     file.write(all_file_list[0]);
-        #     location['main_website'] = all_file_list[0];
         file.write('\n');
 
 location_by_distict = {}
+# by 區域group 各景點: {區域: '', 景點:[]}
 for location in location_info:
     distict = location['distict'];
     view_point = location['stitle'];
@@ -39,8 +34,6 @@ for location in location_info:
         location_by_distict[distict] += [view_point];
     else:
         location_by_distict[distict] = [view_point];
-
-print(location_by_distict);
 
 with open('mrt.csv', mode='w', encoding='utf-8') as file:
     for key, values in location_by_distict.items():
